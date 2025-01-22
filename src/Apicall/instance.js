79600deaf71881df17b91f675b7c46a8235e9257
Vanshaj Gangwar <125118408+vanshaj79@users.noreply.github.com>
2025-01-22 16:07:54 +0530

    Update instance.js

--- src/Apicall/instance.js
+++ src/Apicall/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
  
 const axioInstance = axios.create({
-    baseURL:'https://vs-server-vanshaj-gangwars-projects.vercel.app/',
+    baseURL:'https://vs-server.vercel.app',
     headers: {
         'Content-Type': 'application/json',
     },
